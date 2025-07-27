59628237006a96c8985a7cf50e9fc2586a792137
TheoNapolyIscod <theo.napoly.54918@iscod.school>
2025-07-27 18:50:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ import { HELLO_WORLD_TITLE, HELLO_WORLD_TEXT } from "./constantes.js";
 
 document.title = HELLO_WORLD_TITLE;
 document.getElementById("texte").textContent = HELLO_WORLD_TEXT;
+
+const dateEl = document.getElementById("date");
+dateEl.textContent = new Date().toLocaleDateString();
